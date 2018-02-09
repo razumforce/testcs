cf92d19d5742629720268314a9dd33730fd0935a
razumforce <razum.force@gmail.com>
2018-02-09 15:27:43 +0300

    05 ram samples, constructors

--- myapp/myapp/Program.cs
+++ myapp/myapp/Program.cs
@@ -6,12 +6,60 @@ namespace myapp
     {
         public static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 2;
-            p1.y = 2;
-            p1.sym = '#';
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x = " + x);
 
-            p1.Draw();
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = " + x);
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
+
+            Point p1 = new Point(1, 3, '*');
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+
+            Point p2 = new Point(4, 5, '#');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y);
+
+            p1 = new Point(1, 3, '*');
+            Update(p1);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+        }
+
+
+        public static void Func1(int value)
+        {
+
+        }
+
+        public static void Func2(int value)
+        {
+            value = value + 1;
+        }
+
+        public static void Func3(int x)
+        {
+            x = x + 1;
+        }
+
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        public static void Update(Point p)
+        {
+            p.x = 0;
+            p = new Point(1, 1, '$');
+            Console.WriteLine("arg" + p.x + "arg" + p.y + "sym" + p.sym);
         }
     }
 }
